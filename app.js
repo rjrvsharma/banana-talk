832d930972d88cc9f4791ac84b495f534dcfeedc
Ravishankar Vishwakarma <rjrvsharma@gmail.com>
2021-05-29 18:44:10 +0530

    input and console output

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var btntranslate = document.querySelector("#btn-translate");
 
+var textarea = document.querySelector("#txt-input");
+console.log(textarea);
+
   btntranslate.addEventListener("click", function clickeventhandler() {
     console.log("clicked")
-})  
 
+    console.log("input", textarea.value);
+});
 
