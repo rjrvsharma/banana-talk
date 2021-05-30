439bd3206266ac85e8fa76bc105042cad12d0219
Ravishankar Vishwakarma <rjrvsharma@gmail.com>
2021-05-30 14:04:10 +0530

    app done

--- app.js
+++ app.js
@@ -5,10 +5,31 @@ var txtinput = document.querySelector("#txt-input");
 var outputdiv = document.querySelector("#output");
 
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+  return serverURL + "?" + "text=" + input
+}
+
+
+function errorHandler(error){
+  console.log("error occured",error);
+  alert("something wrong with server! try agin bro")
+}
+
+
 function clickHandler() {
-  outputdiv.innerText = "sdccc " + txtinput.value ;
+ var inputText = txtinput.value;
+
+ fetch(getTranslationURL(inputText))
+ .then(response => response.json())
+ .then(json => {
+   var translatedText= json.contents.translated;
+   outputdiv.innerText = translatedText;
+ })
+ .catch(errorHandler)
 };
 
 btntranslate.addEventListener("click" , clickHandler)
 
-
+ 
